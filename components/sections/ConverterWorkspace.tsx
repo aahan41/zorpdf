4a2a0e58082086ecc598c06ee9953e0f616b1092
aahan41 <mohdirshada9722@gmail.com>
2026-06-15 15:35:40 +0530

    Update ConverterWorkspace.tsx

--- components/sections/ConverterWorkspace.tsx
+++ components/sections/ConverterWorkspace.tsx
@@ -205,8 +205,9 @@ export default function ConverterWorkspace() {
             const result = await convertPdfToImages(pdfFile.file, () => {});
             // Har PDF page ko image ki tarah treat karo
             for (const img of result.images) {
-              const imgBlob = new Blob([await img.blob.arrayBuffer()], { type: 'image/jpeg' });
-            const imgFile = new File([imgBlob], `pdf-page-${img.pageNumber}.jpg`, { type: 'image/jpeg' });, `pdf-page-${img.pageNumber}.jpg`, { type: 'image/jpeg' });
+            const imgBlob = new Blob([img.blob], { type: 'image/jpeg' });
+            const imgFile = new File([imgBlob], `pdf-page-${img.pageNumber}.jpg`, { type: 'image/jpeg' });
+            
               const info = await loadImageInfo(imgFile);
               allImageData.push({
                 id: generateId(),
