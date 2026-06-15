c676c0d2011098f0797c201c3cad870b913a3f71
aahan41 <mohdirshada9722@gmail.com>
2026-06-15 14:18:45 +0530

    Update page.tsx

--- app/admin/dashboard/users/page.tsx
+++ app/admin/dashboard/users/page.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClient } from "@supabase/supabase-js";
-
+import AdminHeader from "@/components/admin/AdminHeader";
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
