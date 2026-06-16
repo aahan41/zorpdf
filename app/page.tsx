d00c947cb1aabd04f551653bfd4ba3592f5176a6
aahan41 <mohdirshada9722@gmail.com>
2026-06-16 16:37:59 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 'use client';
+
 import Navbar from '@/components/sections/Navbar';
 import ConverterWorkspace from '@/components/sections/ConverterWorkspace';
 import FeaturesSection from '@/components/sections/FeaturesSection';
+import Footer from '@/components/sections/Footer';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#050913]">
       <Navbar />
+
       <main>
         <ConverterWorkspace />
         <FeaturesSection />
+        <Footer />
       </main>
     </div>
   );
