d07ae50ebb0d83641a94a2554e3916e8802c7a2c
aahan41 <mohdirshada9722@gmail.com>
2026-08-09 14:43:28 +0530

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from '@/components/sections/Navbar';
 import ConverterWorkspace from '@/components/sections/ConverterWorkspace';
 import FeaturesSection from '@/components/sections/FeaturesSection';
 import HeroSection from '@/components/sections/HeroSection';
+import Footer from '@/components/sections/Footer';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <ConverterWorkspace />
         <FeaturesSection />
       </main>
+      <Footer />
     </div>
   );
 }
